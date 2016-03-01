5dc7a203e2669c288d75fd15321f2f4d2bfe0d1f
JCuevaG <juanrodrigo95@gmail.com>
2016-02-29 22:57:24 -0500

    OK

---
+++ 3DEV.DATA/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using _3DEV.MODELS;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DEV.DATA
+{
+    public class ProductRepository : GenericRepository<Product>
+    {
+        public ProductRepository(DbContext context) : base(context) { }
+    }
+}
