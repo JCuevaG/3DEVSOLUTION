c1756948f83b18ab5072a20c94a84d19384b7c18
JCuevaG <juanrodrigo95@gmail.com>
2016-02-29 22:19:59 -0500

    Ultimos Cambios

--- 3DEV.DATA/GenericRepository.cs
+++ 3DEV.DATA/GenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,47 +10,81 @@ namespace _3DEV.DATA
 {
     public class GenericRepository<T> : IRepository<T> where T : class
     {
-        public DbSet<T> DbSet { get; set; }
-        public DbContext context { get; set; }       
+        protected DbSet<T> DBSet { get; set; }
+        protected DbContext Context { get; set; }
 
-        public GenericRepository(DbContext context)
+        public GenericRepository(DbContext Context)
         {
-            if (context == null)
+            if (Context == null)
             {
-
+                throw new ArgumentException("No Hay Instancia a la DBContext", "Context");
             }
-
-            this.context = context;
-            this.DbSet = this.context.Set<T>();
+            this.Context = Context;
+            this.DBSet = this.Context.Set<T>();
         }
 
+
+        public IQueryable<T> GetAll()
+        {
+            return this.DBSet;
+        }
+
+        public T GetById(int id)
+        {
+            return DBSet.Find(id);
+        }
 
         public void Add(T entity)
         {
-            this.DbSet.Add(entity);
-            this.context.SaveChanges();
-
-        }
-
-        public void Delete(T entity)
-        {
-            this.DbSet.Remove(entity);
-            this.context.SaveChanges();
-        }
-
-        public T FindById(int Id)
-        {
-            return this.DbSet.Find(Id);
+            DbEntityEntry entry = this.Context.Entry(entity);
+            if (entry.State != EntityState.Detached)
+            {
+                entry.State = EntityState.Added;
+            }
+            else
+            {
+                this.DBSet.Add(entity);
+            }
         }
 
         public void Update(T entity)
         {
-            //this.context.SaveChanges();
+            DbEntityEntry entry = this.Context.Entry(entity);
+            if (entry.State != EntityState.Detached)
+            {
+                this.DBSet.Attach(entity);
+            }
+            entry.State = EntityState.Modified;
         }
 
-        public IQueryable<T> List()
+        public void Delete(T entity)
         {
-            return this.DbSet;
+            DbEntityEntry entry = this.Context.Entry(entity);
+            if (entry.State != EntityState.Deleted)
+            {
+                entry.State = EntityState.Deleted;
+            }
+            else
+            {
+                this.DBSet.Attach(entity);
+                this.DBSet.Remove(entity);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var entity = this.GetById(id);
+
+            if (entity != null)
+            {
+                this.Delete(entity);
+            }
+        }
+
+        public void Detach(T entity)
+        {
+            DbEntityEntry entry = this.Context.Entry(entity);
+            entry.State = EntityState.Detached;
         }
     }
 }
