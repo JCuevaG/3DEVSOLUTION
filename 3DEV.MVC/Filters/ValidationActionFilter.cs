2ffc509c39a8fb4343c9d18cd26bdcca77606a02
JCuevaG <juanrodrigo95@gmail.com>
2016-03-13 01:10:01 -0500

    cambios

---
+++ 3DEV.MVC/Filters/ValidationActionFilter.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace _3DEV.MVC.Filters
+{
+    public class ValidationActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            var modelState = actionContext.ModelState;
+            if (!modelState.IsValid)
+            {
+                var errors = new JObject();
+                foreach (var key in modelState.Keys)
+                {
+                    var state = modelState[key];
+                    if (state.Errors.Any())
+                    {
+                        errors[key] = state.Errors.First().ErrorMessage;
+                    }
+                }
+
+                actionContext.Response = actionContext.Request.
+                    CreateResponse<JObject>(HttpStatusCode.BadRequest, errors);
+            }
+        }
+    }
+}
